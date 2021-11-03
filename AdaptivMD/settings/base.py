ff81b9f450fdfcae904c75f51a0bce6885424f5b
Zurab Skhiladze <zurabsx@gmail.com>
2021-11-04 01:23:12 +0400

    first commit

--- AdaptivMD/settings/base.py
+++ AdaptivMD/settings/base.py
@@ -118,10 +118,21 @@ WSGI_APPLICATION = 'AdaptivMD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'adaptiv-psql',
+        'USER': 'adaptiv',
+        'PASSWORD': 'potoli11',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
