6c357ee3455524fb4292ae5b4f3bf237b966fcd5
mbandt <marc@adaptivmd.us>
2021-12-05 03:31:37 +0100

    updated

--- AdaptivMD/settings/dev.py
+++ AdaptivMD/settings/dev.py
@@ -14,7 +14,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'info@adaptivmd.us'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = '8F3Te9=C'
 DEFAULT_FROM_EMAIL = 'info@adaptivmd.us'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SERVER_EMAIL = 'info@adaptivmd.us'
